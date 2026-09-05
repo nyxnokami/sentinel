f00962bf584d6a8600f2315dfcfdf22e62e87f98
nyxnokami <olamia222@gmail.com>
2026-09-05 22:34:39 +0100

    fix:force explicit global cors headers

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,15 @@ import { startWorker } from './workers/poller';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+})
 app.use(express.json());
 
 app.use('/services', servicesRouter);
